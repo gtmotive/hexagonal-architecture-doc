a4ca26a836a833b8fe41f9467f9d57535befd164
VAF PC <kckgvaf@gmail.com>
2023-06-26 19:20:53 +0200

    Prueba de integracion PtRentingTest

---
+++ test/infrastructure/GtMotive.Estimate.Microservice.InfrastructureTests/Infrastructure/PtRentingTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using GtMotive.Estimate.Microservice.Domain.Entities;
+using Newtonsoft.Json;
+using Xunit;
+
+namespace GtMotive.Estimate.Microservice.InfrastructureTests.Infrastructure
+{
+    public class PtRentingTests
+    {
+        [Fact]
+        public async Task PtRentingTestAsync()
+        {
+            using var httpClient = new HttpClient();
+
+            var response = await httpClient.GetAsync(new Uri("https://localhost:51199/api/Renting/ListarVehiculos"));
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+            var responseBody = await response.Content.ReadAsStringAsync();
+            var vehiculos = JsonConvert.DeserializeObject<List<Vehicle>>(responseBody);
+
+            // Asegurar que se haya devuelto una lista de vehículos no vacía
+            Assert.NotEmpty(vehiculos);
+        }
+    }
+}
